ba8d9ddebbdf2a6a4fe89143247cff9bd234af88
sam <samaniloqman91@gmail.com>
2023-12-29 19:32:57 +0100

    alphafold

--- calculus/optimizarion.py
+++ calculus/optimizarion.py
@@ -83,6 +83,7 @@ y_p = W1 * x_p + b
 lr_plot(X, Y, "TV", "Sale", "LR using numpy",W1, b, X_pred=x_p, Y_pred=y_p)
 
 
+
 # Linear Regression with Scikit-Learn
 
 model = LinearRegression()
@@ -113,6 +114,96 @@ print(model.intercept_)
 [[0.04753664]]
 [7.03259355]
 """
+
+
+# Linear regression using gradient descent
+
+
+def cost_func(x, y, m, b):
+
+    loss = 0.5 * np.mean(np.power(m * x + b - y, 2))
+
+    return loss
+
+
+def der_func_dm(x, y, m, b):
+
+    grad = np.dot(m * x + b - y, x) / len(x)
+
+    return grad
+
+
+def der_func_db(x, y, m, b):
+
+    grad = np.mean(m * x + b - y)
+
+    return grad
+
+
+def update(x, y, m, b, num_iter=100, alpha=1e-2, norm=True):
+
+    cost = []
+
+    # Z-Score Normalization: x - mean / std
+    if norm:
+        x = (x - np.mean(x)) / np.std(x)
+        y = (y - np.mean(y)) / np.std(y)
+
+    for i in range(num_iter):
+        loss = cost_func(x=x, y=y, m=m, b=b)
+        m_grad = der_func_dm(x=x, y=y, m=m, b=b)
+        b_grad = der_func_db(x=x, y=y, m=m, b=b)
+
+        m -= alpha * m_grad
+        b -= alpha * b_grad
+        cost.append(loss)
+
+    return m, b, cost
+
+
+slope, intercept, cost = update(x=X, y=Y, m=0, b=0, alpha=1.2, num_iter=30, norm=True)
+
+print(slope)
+print(intercept)
+print(cost)
+"""
+0.7822244248616065
+-3.19744231092045e-16
+[0.5, 0.2062999755919659, 0.19455197461564455, ..., 0.19406247457496448, 0.19406247457496448]
+"""
+
+plt.plot(cost)
+plt.show()
+
+slope1, intercept1, cost1 = update(x=X, y=Y, m=0, b=0, num_iter=200)
+
+print(slope1)
+print(intercept1)
+print(cost1)
+"""
+0.677422250752692
+-2.0099477637813844e-16
+[0.5, 0.4939118432440418, 0.48794484080752715,..., 0.19977949587404065, 0.19966572715018904]
+"""
+
+plt.plot(cost1)
+plt.show()
+
+slope2, intercept2, cost2 = update(x=X, y=Y, m=2, b=2, alpha=0.1, num_iter=30)
+
+print(slope2)
+print(intercept2)
+print(cost2)
+"""
+0.8338473420109904
+0.08478231655043211
+[2.9355511502767877, 2.414668301893441,..., 0.20157121316258064, 0.20014455283093355] 
+"""
+
+plt.plot(cost2)
+plt.show()
+
+
 
 
 
