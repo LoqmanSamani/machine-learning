f70fdd5e239901a445a0e2eb6a5884a8b7ded293
sam <samaniloqman91@gmail.com>
2024-01-03 16:15:53 +0100

    machine learning

---
+++ models/model4.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from matplotlib import colors
+from sklearn.datasets import make_blobs
+
+
+# A model (neural network) with two layers to solve a classification problem
+
+
+class Classification(object):
+
+    def __init__(self):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+np.random.seed(3)
